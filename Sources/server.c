2b26545a470d7b55480758922645c4409a9575d7
Valentin CRÔNE <valentin.crone@telecomnancy.eu>
2018-04-11 15:38:34 +0200

    Update des noms de clé pour se conformer à règles de la nouvelle structure interne

--- Sources/server.c
+++ Sources/server.c
@@ -4,9 +4,9 @@ int main()
 {
 	JSONObject_t bdd = JSONObject_new();
 	JSONObject_t films = JSONParser_parseFile("data/bd.json");
-	JSONObject_set(bdd, autoString("films"), films);
+	JSONObject_set(bdd, autoString("Films"), films);
 	JSONArray_t users = JSONArray_new();
-	JSONObject_set(bdd, autoString("users"), users);
+	JSONObject_set(bdd, autoString("Users"), users);
 	printf("%s\n", cString(JSONObject_asString(bdd, 0)));
 	bdd = JSONObject_delete(bdd);
 	freeAutoString();
