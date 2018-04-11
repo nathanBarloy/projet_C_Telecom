828c0e69a8e9683bc3b5bef1dc55503ce8d4b0df
Valentin CRÔNE <valentin.crone@telecomnancy.eu>
2018-04-11 10:14:21 +0200

    Ajout du fichier manquant pour JSONC

---
+++ Sources/include/cjson/JSONAll.h
@@ -0,0 +1,12 @@
+#ifndef JSONALL_H
+#define JSONALL_H
+#include "JSONObject.h"
+#include "JSONTuple.h"
+#include "JSONParser.h"
+#include "JSONInt.h"
+#include "JSONDouble.h"
+#include "JSONNull.h"
+#include "JSONString.h"
+#include "JSONValue.h"
+#include "JSONArray.h"
+#endif
