e52adbea7d1126deed81f9a04a3701f5bd00a185
Valentin CRÔNE <valentin.crone@telecomnancy.eu>
2018-04-11 10:18:32 +0200

    Ajout d'une version de test de dépendances de libraries pour server

--- Sources/server.c
+++ Sources/server.c
@@ -1,4 +1,14 @@
+#include <cjson/JSONAll.h>
+#include <String/AutoString.h>
 int main()
 {
+	JSONObject_t bdd = JSONObject_new();
+	JSONObject_t films = JSONParser_parseFile("data/bd.json");
+	JSONObject_set(bdd, autoString("films"), films);
+	JSONArray_t users = JSONArray_new();
+	JSONObject_set(bdd, autoString("users"), users);
+	printf("%s\n", cString(JSONObject_asString(bdd, 0)));
+	bdd = JSONObject_delete(bdd);
+	freeAutoString();
 	return 0;
 }
