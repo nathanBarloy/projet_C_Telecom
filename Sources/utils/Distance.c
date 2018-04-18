cd53313498d4f2e32d35aac80d004dbe656ac796
nathan.barloy@telecomnancy.eu <nathan.barloy@telecomnancy.eu>
2018-04-18 20:17:46 +0200

    prototype de distance plus complet

--- Sources/utils/Distance.c
+++ Sources/utils/Distance.c
@@ -10,12 +10,53 @@ float distance(BDD bdd,int id1, int id2) {
 	JSONArray_t films = BDD_Films(bdd);
 	JSONObject_t film1 = JSONArray_get(films, id1);
 	JSONObject_t film2 = JSONArray_get(films, id2);
+	float coeffActor=3,coeffGenre=5,coeffReal=3,coeffType=1,coeffYear=1;
+	//float distActor = distance_actor();
+	//float distGenre = distance_genre();
+	//float distReal = distance_real();
+	float distActor = 0;
+	float distGenre = 0;
+	float distReal = 0;
+	float distType = distance_type(JSONObject_stringValueOf(film1,autoString("Type")),JSONObject_stringValueOf(film2,autoString("Type")));
+	float distYear = distance_year(JSONObject_intValueOf(film1,autoString("Year")),JSONObject_intValueOf(film2,autoString("Year")));
+
+	dist = (coeffActor*distActor+coeffGenre*distGenre+coeffReal*distReal+coeffType*distType+coeffYear*distYear ) / (coeffActor+coeffReal+coeffType+coeffYear+coeffGenre);
 
 	printf("la distance entre ");
-	printf("%s et ", cString(JSONObject_asString(JSONObject_get(film1,autoString("Title")),0)));
-	printf("%s est ", cString(JSONObject_asString(JSONObject_get(film2,autoString("Title")),0)));
+	printf("%s et ", cString(JSONObject_stringValueOf(film1,autoString("Title"))));
+	printf("%s et ", cString(JSONObject_stringValueOf(film2,autoString("Title"))));
 	printf("%f\n",dist);
 
 
 	return dist;
+}
+
+float distance_year(int y1,int y2) {
+  float dist = 0;
+
+  return dist;
+}
+
+float distance_genre(char **l1, char **l2) {
+  float dist = 0;
+
+  return dist;
+}
+
+float distance_actor(char **l1, char **l2) {
+  float dist = 0;
+
+  return dist;
+}
+
+float distance_real(char **l1, char **l2) {
+  float dist = 0;
+
+  return dist;
+}
+
+float distance_type(String_t t1, String_t t2) {
+  float dist = 0;
+
+  return dist;
 }
