bc0f1b470b3616591c42e7fbb31d53768a83fb0d
Valentin CRÔNE <valentin.crone@telecomnancy.eu>
2018-05-09 10:54:01 +0200

    Update client/server

--- Sources/client-gui.c
+++ Sources/client-gui.c
@@ -4,12 +4,15 @@
 #include "sys/ClientRequests.h"
 #include "utils/Connexion.h"
 #include "utils/JSONShortcut.h"
+#include <gtk/gtk.h>
+#include <webkit/webkit.h>
+
 int main(int argc, char** argv, char** envp)
 {
-	if(argc >= 2)
+	/*if(argc >= 2)
 	{
 		initClient();
-		Connexion_t connexion = newConnexion(argv[1]);//Adresse du serveur
+		Connexion_t connexion = newConnexion(argv[1]);*///Adresse du serveur
 		//prints(connexion, autoString("Bonjour tout le monde !"));
 
 
@@ -23,12 +26,27 @@ int main(int argc, char** argv, char** envp)
 
 		//int r = clientRunner(connexion);
 		//Fin
-		connexion = freeConnexion(connexion);
+		/* Create the widgets */
+		GtkWidget *main_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+		GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+		GtkWidget *web_view = webkit_web_view_new();
+
+		/* Place the WebKitWebView in the GtkScrolledWindow */
+		gtk_container_add (GTK_CONTAINER (scrolled_window), web_view);
+		gtk_container_add (GTK_CONTAINER (main_window), scrolled_window);
+
+		/* Open a webpage */
+		webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web_view), "http://www.gnome.org");
+
+		/* Show the result */
+		gtk_window_set_default_size(GTK_WINDOW (main_window), 800, 600);
+		gtk_widget_show_all(main_window);
+	/*	connexion = freeConnexion(connexion);
 	}
 	else
 	{
 		printf("Usage: client [server_ip]\n");
 	}
-	freeAutoString();
+	freeAutoString();*/
 	return 0;
 }
