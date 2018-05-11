9345e90d17a1609c3b0ba359585b1315d4ba6618
Valentin CRÔNE <valentin.crone@telecomnancy.eu>
2018-05-11 03:06:45 +0200

    Add test image

--- Sources/gui/GUIHandleWebKit.c
+++ Sources/gui/GUIHandleWebKit.c
@@ -10,6 +10,9 @@
 #include "../utils/Headers.h"
 #include <string.h>
 #include <glib-object.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 bool clientGUI_blockableRequest = 1;
 bool clientGUI_firstRequest = 1;
 void clientGUIHeadGet(const char* name, const char* value, gpointer user_data)
@@ -26,18 +29,18 @@ void clientGUIRessourceRequestStarting(WebKitWebView* web_view, WebKitWebFrame *
 	printf("Frame: %p\n", web_frame);
 	printf("Resource: %p\n", web_resource);
 	printf("Request: %p\n", request);
+	printf("FrameURI: %s\n", webkit_web_frame_get_uri(web_frame));
 	printf("Response: %p\n", response);
 	printf("URI: %s\n", webkit_network_request_get_uri(request));
-	AutoString_t uri = autoString((char*)webkit_network_request_get_uri(request));
+	printf("Print end.\n");
+	char *cfuri = (char*) webkit_web_frame_get_uri(web_frame);
+	AutoString_t uri = autoString((char*)webkit_network_request_get_uri(request)), furi = autoString(cfuri != 0 ? cfuri : "");
 	if(sizeOfString(uri) > 0 && uri->str[0] != 'e')
 	{
 		clientGUI_blockableRequest = false;
 	}
 	if(clientGUI_blockableRequest && !clientGUI_firstRequest)
 	{
-		//Annulation de la requête
-		webkit_web_view_stop_loading(web_view);
-		printf("Request blocked: %s\n", cString(uri));
 		clientGUI_blockableRequest = 0;
 		//Reception des paramètres envoyés
 		/*SoupMessage* msg = webkit_network_request_get_message(request);
@@ -70,12 +73,86 @@ void clientGUIRessourceRequestStarting(WebKitWebView* web_view, WebKitWebFrame *
 
 		}*/
 		//Génération HTML
-		String_t html = HTMLFromJSONUrl(connexion, uri);
 		//freeVectorWithPtr(headers, freeHeaderPtr);
-		printf("Generated:\n%s\n", cString(html));
 		//Génération de la nouvelle requête
-		webkit_web_view_load_string(WEBKIT_WEB_VIEW(web_view), cString(html) , 0,0, cString(uri));//Chargement via generateur HTML
-		fString(html);
+		size_t c = sizeOfString(uri) - 1;
+		while(c > 0)
+		{
+			if(uri->str[c] == '.')
+			{
+				++c;
+				break;
+			}
+			--c;
+		}
+		String_t mt = 0;
+		bool isExec = true;
+		if(c > 0)
+		{
+			if(strcmp(uri->str + c, "png") == 0)
+			{
+				mt = newStringFromCharStar("image/png");
+			}
+			else if(strcmp(uri->str + c, "jpg") == 0)
+			{
+				mt = newStringFromCharStar("image/jpeg");
+			}
+			if(mt != 0)
+			{
+				isExec = false;
+			}
+		}
+		if(isExec)
+		{
+			webkit_web_view_stop_loading(web_view);
+			printf("Request blocked: %s\n", cString(uri));
+			String_t html = HTMLFromJSONUrl(connexion, uri);
+			printf("Generated:\n%s\n", cString(html));
+			webkit_web_view_load_string(WEBKIT_WEB_VIEW(web_view), cString(html) , 0,0, cString(uri));//Chargement via generateur HTML
+			fString(html);
+		}
+		else
+		{
+			if(sizeOfString(uri) >= sizeOfString(autoString(("exec://"))))
+			{
+				String_t file = 0;
+				if(sizeOfString(uri) >= sizeOfString(furi))
+				{
+					file = newStringFromCharStar(uri->str + sizeOfString(furi));
+				}
+				else
+				{
+					file = newStringFromCharStar(uri->str + sizeOfString(autoString("exec://")));
+				}
+				size_t st = 10000001;
+				char *buf = (char*) malloc(sizeof(char) * st);
+				buf[0] = 0;
+				char *df = cString(autoConcatString(autoString("web/"), file));
+				printf("Reading file: %s\n", df);
+				int fd = open(df, O_RDONLY);
+				if(fd != -1)
+				{
+					size_t r = read(fd, buf, st - 1);
+					printf("Read size: %lu\n", r);
+					buf[r] = 0;
+					close(fd);
+					//webkit_web_view_stop_loading(web_view);
+					printf("Request blocked: %s\n", cString(uri));
+					printf("Requesting: %s\n", df);
+					//webkit_web_frame_load_string(WEBKIT_WEB_FRAME(web_frame), buf , cString(mt),0, cString(uri));//Chargement via generateur HTML
+					free(buf);
+				}
+				else
+				{
+					printf("Error while reading file: %s\n", df);
+				}
+			}
+			else
+			{
+				printf("Loading of: %s → Cancelled.", cString(uri));
+			}
+			fString(mt);
+		}
 	}
 	else
 	{
