348ff8885f3f557a2f9f70ef18fbfd49c8f60a1b
Johan TOMBRE <johan.tombre@telecomnancy.eu>
2018-05-14 23:44:50 +0200

    Script python téléchargeant les affiches des films + les 100 images

--- Recherches/AfficheFilms/script.py
+++ Recherches/AfficheFilms/script.py
@@ -8,29 +8,63 @@ import simplejson as json
 with open('../ReadBD/bd.json') as bd:
     config = json.load(bd)
 
-# genère l'url de la requête
-def URLgenerator(title):
-    query = title + " cover"
+def MovieDBUrl(id):
+    title = config[id]["Title"]
+    # préparation de la requête
     request = ""
-    for letter in query:
+    for letter in title:
         if letter == " ":
             request = request + "+"
         elif letter == "&":
             request = request + "%26"
         else:
             request = request + letter
-    url = 'https://www.google.com/search?q=' + request + '&tbm=isch'
-    print(url)
-    return url
+    # Film ou TV series
+    type = config[id]["Type"]
+    if type == "Film":
+        cat = "movie"
+    elif type == "TV Series" or type == "TV Movie":
+        cat = "tv"
+    # Finalisation de l'URL
+    url = 'https://www.themoviedb.org/search/' + cat +'?query=' + request
+    # print(url)
+    return url, cat
+
 
 # problème de avec .text (ne lui pas la page html comme souhaité)
 def selectLink(url):
+    html_content = requests.get(url[0]).text
+    # Film ou TV series
+    search_result = regex.findall(r'href=\"/' + url[1] + '/(.*?)\" title', html_content)
+    # search_result = regex.findall(r'/film/fichefilm_gen_cfilm=198750.html', html_content)
+    print("On a " + str(len(search_result)) + " résultat(s)")
+    # for i in range(len(search_result)):
+    #     print(search_result[i])
+    return search_result, url[1]
+
+def checkDate(url, id):
+    # print(url)
     html_content = requests.get(url).text
-    print(html_content)
-    search_result = regex.findall(r'href=\"\/imgres\?imgurl=(.*?)&imgrefurl.*?', html_content)
-    print(len(search_result))
+    date = regex.findall(r'class=\"release_date\">\((.{4})\)', html_content)
+    if date[0] == str(config[id]["Year"]):
+        return 1
+    else:
+        return 0
 
+def getImageURL(url):
+    # print(page)
+    html_content = requests.get(url).text
+    result = regex.findall(r'<img class="poster".*?srcset=.*?1x, https://(.*?).jpg 2x', html_content)
+    # print(result[0])
+    return result[0]
 
+def downloadImage(url, id):
+    newUrl = "https://" + url + ".jpg"
+    r = requests.get(newUrl)
+    open('./../../Sources/web/img/covers/'+ str(id) + '.jpg', 'wb').write(r.content)
+    print("Image téléchargé pour " + config[id]["Title"])
+
+# obtenir les infos (dimensions de l'image) pour avoir une qualité suffisante
 def getSizes(url):
     # get file size *and* image size (None if not known)
     file = requests.get(url)
@@ -50,17 +84,20 @@ def getSizes(url):
 
 
 
-
 # main
-print(config[0]["Title"])
-URLgenerator(config[0]["Title"])
-selectLink(URLgenerator(config[0]["Title"]))
-print(getSizes('http://www.zickma.fr/wp-content/uploads/2015/06/Fast-7-extended.jpg'))
-
-# requête pour accéder au résultat des recherches images
-
-# sélection de la première image répondant aux conditions :
-#    largueur < hauteur
-#    Mot Affiche dans le Mot
-
-# Télécharger le fichier et le stocker dans un dossier
+for i in range(85,100):
+    urlRequest = MovieDBUrl(i)
+    answer = selectLink(urlRequest)
+    urlPart = answer[0]
+    type = answer[1]
+    j = 0
+    cnt = 0
+    last = ""
+    while j==0:
+        if last != urlPart[cnt]:
+            page = "https://www.themoviedb.org/" + type + "/" + urlPart[cnt]
+            j = checkDate(page, i)
+            last = urlPart[cnt]
+        cnt = cnt + 1
+    print("la bonne page est : " + page)
+    downloadImage(getImageURL(page), i)
