99b8a70ac3d0a7a20deed27ef24c6f031df57bc9
Johan TOMBRE <johan.tombre@telecomnancy.eu>
2018-05-16 17:22:40 +0200

    Modification de la bdd (destination.json) pour y inclure lien vers les affiches de films

--- Recherches/LiensBandeAnnonce/bdd_impl.py
+++ Recherches/LiensBandeAnnonce/bdd_impl.py
@@ -5,13 +5,16 @@ rsc = open("liensYouTube.txt", "r")
 dest = open("destination.json", "w")
 
 rsc_line = rsc.readline()
+i = 0;
 for line in file:
     m = re.match(".\"Year\".", line)
     if(m):
         dest.write(line.rstrip('\n')[:-1] + ",\n")
         link = re.sub('[0-9]{1,3}: https', 'https', rsc_line)
-        dest.write("  \"Url\": \"" + link.rstrip('\n') + "\"\n")
+        dest.write("    \"Url\": \"" + link.rstrip('\n') + "\",\n")
         rsc_line = rsc.readline()
+        dest.write("    \"Cover\": \"img/covers/" + str(i) + ".jpg\"\n")
+        i = i+1
     else:
         dest.write(line)
 
